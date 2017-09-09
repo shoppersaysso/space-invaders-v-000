11d53b8b9780eb37aa1f503360139c0718d07c42
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 03:42:48 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -3,7 +3,9 @@ constructor(position) {
   this.position = position;
   this.currentShip = 'None';
   }
-
+  // if (this.assignment = 'none') {
+  //   return "Looking for a Rig";
+  // }
 }
 
 class Spaceship {
