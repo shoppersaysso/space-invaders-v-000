b61c93c388ae7d47428cc1c3f695af22ad49981b
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 03:45:19 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -23,28 +23,3 @@ notifyCrew(){
     });
   }
 }
-
-
-class Spaceship {
-  constructor(name, crew, phasers, shields){
-    this.name = name;
-    this.crew = crew;
-    this.phasers = phasers;
-    this.shields = shields;
-    this.cloaked = false;
-    this.warpDrive = 'disengaged';
-    if (crew.length === 0){
-      this.docked = true;
-    }else {
-      this.docked = false;
-    };
-    this.phasersCharge = "uncharged";
-    this.assignCrew();
-  }
-
-  assignCrew(){
-    this.crew.forEach(CrewMember => {
-      CrewMember.currentShip = this;
-    })
-  }
-}
