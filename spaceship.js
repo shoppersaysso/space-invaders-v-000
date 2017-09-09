27a0c6ee27705cb39c9eb204b6e8af98b870772d
Sara Hopper <sara.hopper@gmail.com>
2017-09-09 03:35:49 +0000

    automatically backed up by learn

--- spaceship.js
+++ spaceship.js
@@ -22,3 +22,14 @@ constructor(name, crew, phasers, shields) {
   this.notifyCrew();
   }
 }
+
+notifyCrew(){
+    // using an arrow function lexically
+    // binds `this` to the context of this
+    // instance of Spaceship, rather than
+    // the context of the callback
+    this.crew.forEach(crewMember => {
+      crewMember.currentShip = this;
+    });
+  }
+}
